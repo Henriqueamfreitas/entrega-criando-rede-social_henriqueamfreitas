c989f950805d6ea4c4e67ad93eea31d2bdd75d0e
Henrique <you@example.com>
2024-01-09 16:26:57 -0300

    ajustes finais

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,25 +1,23 @@
-// Definining and creating the principal HTML elements
-const body = document.querySelector("body")
-const main = document.createElement("main")
-const aside = document.createElement("aside")
-const div = document.createElement("div")
+const body = document.querySelector('body')
+const main = document.createElement('main')
+const aside = document.createElement('aside')
+const div = document.createElement('div')
 
-const header = document.createElement("header")
-const h1 = document.createElement("h1")
-const button = document.createElement("button")
-h1.innerHTML="TechPosters"
-button.innerHTML="Sair"
-button.classList="text-2"
+const header = document.createElement('header')
+const h1 = document.createElement('h1')
+const button = document.createElement('button')
+h1.innerHTML='TechPosters'
+button.innerHTML='Sair'
+button.classList='text-2'
 body.append(header)
 header.append(h1, button)
 body.append(createNewPost(users))
 
-// Creating function that render the cards
 function render(array, name, section, sectionString, functionName) {
-    const h1 = document.createElement("h1")
+    const h1 = document.createElement('h1')
     h1.innerHTML=name
     h1.classList= `${sectionString}__h1 title-1`
-    const list = document.createElement("ul")
+    const list = document.createElement('ul')
     list.classList= `${sectionString}__ul`
 
   
@@ -36,22 +34,20 @@ function render(array, name, section, sectionString, functionName) {
     section.append(h1, list)
 }
 
-// Creating function that creates new posts
 function createNewPost(users){
-  // Creating elements
-  const newPostSection = document.createElement("section") 
-  const figure = document.createElement("figure")
-  const userImg = document.createElement("img")
-  const figcaption = document.createElement("figcaption")
-  const name = document.createElement("p")
-  const description = document.createElement("p")
+  const newPostSection = document.createElement('section') 
+  const figure = document.createElement('figure')
+  const userImg = document.createElement('img')
+  const figcaption = document.createElement('figcaption')
+  const name = document.createElement('p')
+  const description = document.createElement('p')
 
-  const newPostInputs = document.createElement("div")
-  const newPostTitle = document.createElement("input")
-  const newPostDescription = document.createElement("input")
-  const button = document.createElement("button")
+  const form = document.createElement('form')
+  const newPostTitle = document.createElement('input')
+  const newPostDescription = document.createElement('input')
+  const div = document.createElement('div')
+  const button = document.createElement('button')
 
-  // Assigning classes to the elements
   newPostSection.classList=`newPostSection`
   figure.classList=`newPostSection__figure`
   userImg.classList=`profile__img`
@@ -59,53 +55,91 @@ function createNewPost(users){
   name.classList=`profile-figcaption__name title-2`
   description.classList=`profile-figcaption__description text-2`
 
-  newPostInputs.classList=`newPost__inputsPart`
+  form.classList=`newPost__form`
   newPostTitle.classList=`newPostSection__postTitle`
   newPostDescription.classList=`newPostSection__postDescription`
+  div.classList='newPost__buttonPart'
   button.classList=`newPostSection__button text-4`
 
-  // Assigining values to the elements
   const user=users[0]
   userImg.src=user.img 
   name.innerHTML=user.user 
   description.innerHTML=user.stack 
 
-  newPostTitle.placeholder="Digitar título do post"
-  newPostDescription.placeholder="Digitar descrição do post"
-  button.innerHTML="Postar" 
+  newPostTitle.placeholder='Digitar título do post'
+  newPostDescription.placeholder='Digitar descrição do post'
+  newPostTitle.required=true
+  newPostDescription.required=true
+  button.innerHTML='Postar' 
+  button.type='submit'
 
-  // Establishing the hierarchy between elements
-  newPostSection.append(figure, newPostTitle, newPostDescription, newPostInputs)
-  newPostInputs.append(button)
+  
+  newPostSection.append(figure, newPostTitle, newPostDescription, form)
+  form.append(newPostTitle, newPostDescription, div)
+  div.append(button)
   figure.append(userImg, figcaption)
   figcaption.append(name, description)
+  
 
+  form.addEventListener('keyup', ()=>{
+    if((newPostTitle.value !== '') && (newPostDescription.value !== '')){
+      button.style.backgroundColor='#4263EB'
+      button.style.color='#FFFFFF'
+    } else{
+      button.style.backgroundColor='#DEE2E6'
+      button.style.color='#868E96'
+    }
+  })
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+  
+    if (newPostTitle.value && newPostDescription.value) {
+        const newPost = {
+        id: posts.length + 1,
+        title: newPostTitle.value,
+        text: newPostDescription.value.substring(0,199),
+        fullText: newPostDescription.value,
+        user: user.user, 
+        stack: user.stack, 
+        img: user.img, 
+        likes: 0
+    }
+  
+      posts.push(newPost);
+
+      newPostTitle.value = '';
+      newPostDescription.value = '';
+  
+      const mainSection = document.querySelector('main');
+      mainSection.innerHTML = ''; 
+      render(posts, 'Posts', mainSection, 'main', createCard);
+      showPostModal(posts);
+    }
+  })
+  
   return newPostSection
 }
 
-
-// Creating function that render the posts
 function createCard(posts, sectionString){
-    // Creating elements
-    const li = document.createElement("li")
-    const figure = document.createElement("figure")
-    const userImg = document.createElement("img")
-    const figcaption = document.createElement("figcaption")
-    const name = document.createElement("p")
-    const description = document.createElement("p")
+    const li = document.createElement('li')
+    const figure = document.createElement('figure')
+    const userImg = document.createElement('img')
+    const figcaption = document.createElement('figcaption')
+    const name = document.createElement('p')
+    const description = document.createElement('p')
 
-    const postDescription = document.createElement("div")
-    const h2 = document.createElement("h2")
-    const postText = document.createElement("p")
+    const postDescription = document.createElement('div')
+    const h2 = document.createElement('h2')
+    const postText = document.createElement('p')
 
-    const postLikes = document.createElement("div")
-    const button = document.createElement("button")
-    const likeButton = document.createElement("button")
-    const likesInformation = document.createElement("div")
-    const heartImage = document.createElement("img")
-    const numberOfLikes = document.createElement("p")
+    const postLikes = document.createElement('div')
+    const button = document.createElement('button')
+    const likeButton = document.createElement('button')
+    const likesInformation = document.createElement('div')
+    const heartImage = document.createElement('img')
+    const numberOfLikes = document.createElement('p')
 
-    // Assigning classes to the elements
     li.classList=`${sectionString}__li`
     figure.classList=`${sectionString}__figure`
     userImg.classList=`figure__img`
@@ -124,10 +158,8 @@ function createCard(posts, sectionString){
     heartImage.classList=`postLikes__heartImage`
     numberOfLikes.classList=`postLikes__numberOfLikes text-2`
 
-    // Assigning IDs to the open buttons
     button.dataset.postsId=posts.id
 
-    // Assigining values to the elements
     userImg.src=posts.img 
     name.innerHTML=posts.user 
     description.innerHTML=posts.stack 
@@ -135,11 +167,10 @@ function createCard(posts, sectionString){
     h2.innerHTML=posts.title 
     postText.innerHTML=posts.text 
 
-    button.innerHTML="Abrir post" 
-    heartImage.src="./src/assets/img/greyHeart.svg" 
+    button.innerHTML='Abrir post' 
+    heartImage.src='./src/assets/img/greyHeart.svg' 
     numberOfLikes.innerHTML=posts.likes 
 
-    // Establishing the hierarchy between elements
     li.append(figure, postDescription, postLikes)
     figure.append(userImg, figcaption)
     figcaption.append(name, description)
@@ -148,26 +179,37 @@ function createCard(posts, sectionString){
     likesInformation.append(likeButton, numberOfLikes)
     likeButton.append(heartImage)
 
+    let clicked=false
+    likeButton.addEventListener('click', ()=>{
+      clicked = !clicked
+      if (clicked) {
+        let updatedNumberOfLikes=0
+        updatedNumberOfLikes += posts.likes+1
+        numberOfLikes.innerHTML = updatedNumberOfLikes
+        heartImage.src='./src/assets/img/redHeart.svg' 
+      } else {
+        numberOfLikes.innerHTML = posts.likes
+        heartImage.src='./src/assets/img/greyHeart.svg' 
+      }
+    })
+
     return li
 }
 
-// Creating function that render the Modal posts
 function createModalCard(posts, sectionString){
-  // Creating elements
-  const closeButton = document.createElement("button")
+  const closeButton = document.createElement('button')
 
-  const li = document.createElement("li")
-  const figure = document.createElement("figure")
-  const userImg = document.createElement("img")
-  const figcaption = document.createElement("figcaption")
-  const name = document.createElement("p")
-  const description = document.createElement("p")
+  const li = document.createElement('li')
+  const figure = document.createElement('figure')
+  const userImg = document.createElement('img')
+  const figcaption = document.createElement('figcaption')
+  const name = document.createElement('p')
+  const description = document.createElement('p')
 
-  const postDescription = document.createElement("div")
-  const h2 = document.createElement("h2")
-  const postText = document.createElement("p")
+  const postDescription = document.createElement('div')
+  const h2 = document.createElement('h2')
+  const postText = document.createElement('p')
 
-  // Assigning classes to the elements
   closeButton.classList=`modal__closeButton`
   
   li.classList=`modal__li`
@@ -181,8 +223,7 @@ function createModalCard(posts, sectionString){
   h2.classList=`modal-postDescription__h2 title-1`
   postText.classList=`modal-postDescription__postText text-1`
 
-  // Assigining values to the elements
-  closeButton.innerHTML="X"
+  closeButton.innerHTML='X'
 
   userImg.src=posts.img 
   name.innerHTML=posts.user 
@@ -191,7 +232,6 @@ function createModalCard(posts, sectionString){
   h2.innerHTML=posts.title 
   postText.innerHTML=posts.fullText 
 
-  // Establishing the hierarchy between elements
   li.append(closeButton, figure, postDescription)
   figure.append(userImg, figcaption)
   figcaption.append(name, description)
@@ -200,33 +240,44 @@ function createModalCard(posts, sectionString){
   return li
 }
 
-// Creating function that render the friends sugestions cards
 function createCardSugestion(users, sectionString){
-  // Creating elements
-  const li = document.createElement("li")
-  const figure = document.createElement("figure")
-  const userImg = document.createElement("img")
-  const figcaption = document.createElement("figcaption")
-  const name = document.createElement("p")
-  const description = document.createElement("p")
-  const button = document.createElement("button")
+  const li = document.createElement('li')
+  const figure = document.createElement('figure')
+  const userImg = document.createElement('img')
+  const figcaption = document.createElement('figcaption')
+  const name = document.createElement('p')
+  const description = document.createElement('p')
+  const button = document.createElement('button')
   
-  // Assigning classes to the elements
   li.classList=`${sectionString}__li`
   figure.classList=`${sectionString}__figure`
   userImg.classList=`figure__img`
   figcaption.classList=`figure__figcaption`
   name.classList=`figcaption__name title-2`
   description.classList=`figcaption__description text-2`
-  button.classList="text-4"
+  button.classList='text-4'
 
-  // Assigining values to the elements
   userImg.src=users.img 
   name.innerHTML=users.user 
   description.innerHTML=users.stack 
-  button.innerHTML="Seguir"
+  button.innerHTML='Seguir'
 
-  // Establishing the hierarchy between elements
+  let clicked=false
+  button.addEventListener('click', ()=>{
+    clicked=!clicked
+    if(clicked){
+      button.innerHTML='Seguindo'
+      button.style.backgroundColor='#212529'
+      button.style.color='#FFFFFF'
+      button.style.borderColor='#FFFFFF'
+    }else{
+      button.innerHTML='Seguir'
+      button.style.backgroundColor='#FFFFFF'
+      button.style.color='#212529'
+      button.style.borderColor='#212529'
+    }  
+  })
+
   li.append(figure, button)
   figure.append(userImg, figcaption)
   figcaption.append(name, description)
@@ -234,29 +285,28 @@ function createCardSugestion(users, sectionString){
   return li
 }
 
-// Modal
 function closeModal(){
-  const button = document.querySelector(".modal__closeButton")
-  const modalController = document.querySelector(".modal-item__controller")
+  const button = document.querySelector('.modal__closeButton')
+  const modalController = document.querySelector('.modal-item__controller')
   
-  button.addEventListener("click", ()=>{
+  button.addEventListener('click', ()=>{
     modalController.close()
   })
 }
 
 function showPostModal(array){
-  const modalController = document.querySelector(".modal-item__controller")
-  const buttons = document.querySelectorAll(".main__ul > .main__li > .main__postLikes > .postLikes__button")
+  const modalController = document.querySelector('.modal-item__controller')
+  const buttons = document.querySelectorAll('.main__ul > .main__li > .main__postLikes > .postLikes__button')
 
   for (let i=0; i<buttons.length; i++){
     const button = buttons[i]
 
-    button.addEventListener("click", (event)=>{
-      modalController.innerHTML=""
+    button.addEventListener('click', (event)=>{
+      modalController.innerHTML=''
       let id = event.target.dataset.postsId
       let post = findPost(array, id)
       const modalCard = createModalCard(post)
-      const modalList = document.createElement("ul")
+      const modalList = document.createElement('ul')
       modalList.append(modalCard)
       modalController.append(modalList) 
 
@@ -279,6 +329,6 @@ function findPost(array, id){
 }
 
 createNewPost(users)
-render(suggestUsers, "Sugestões para você seguir", aside, "aside", createCardSugestion)
-render(posts, "Posts", main, "main", createCard)
+render(suggestUsers, 'Sugestões para você seguir', aside, 'aside', createCardSugestion)
+render(posts, 'Posts', main, 'main', createCard)
 showPostModal(posts)
